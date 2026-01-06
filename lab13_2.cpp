266c00d7d03ef4fbbef5ac702663c66d7a8739c5
Thiw <wiriyaphat6789@gmail.com>
2026-01-06 16:00:36 +0700

    Lab13_2 complete

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,25 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool imag[][M],int s,int x,int y){
+    for(int i = 0; i < N;i++){
+        for(int j = 0; j < M; j++){
+            if (sqrt(pow((i - x),2)+pow((j - y),2)) <= s-1){
+            imag[i][j] = 1;}
+        }
+    }
+}
+void showImage(const bool imag[][M]){
+    cout << "------------------------------------------------------------------------" << endl;
+    for(int i = 0; i < 30;i++){
+        cout << "|";
+        for(int j = 0; j < 70; j++){
+            if (imag[i][j] == 1){cout << '*';}
+            else {cout << " ";} 
+        }
+        cout << "|";
+        cout << endl;
+    }
+    cout << "------------------------------------------------------------------------" << endl;
+}
