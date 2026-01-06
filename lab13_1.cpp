6b1251c65eaeab1c481db9d76b6ca30bd293076b
Thiw <wiriyaphat6789@gmail.com>
2026-01-06 15:19:30 +0700

    Lab13_1 complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,32 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat (const double x[], int y, double z[]){
+    double sum = 0, summ = 0, mul = 1, sumR = 0, Max = 0,Min = x[0];
+    for(int i = 0;i < y; i++){
+        sum += x[i];
+    }
+    z[0] = sum/y;
+
+    for (int i = 0;i < y;i++){
+        summ += pow((x[i] - z[0]),2);
+    }
+    z[1] = sqrt((summ/y));
+    
+    for (int i = 0;i < y;i++){
+        mul *= x[i];
+    }
+    z[2] = pow(mul, 1.0/y);
+    
+    for(int i = 0; i<y; i++){
+        sumR += 1/x[i];
+    }
+    z[3] = y/sumR;
+
+    for(int i = 0; i<y; i++){
+        if (x[i] > Max){Max = x[i];}
+        if (x[i] < Min){Min = x[i];}
+    }
+    z[4] = Max;
+    z[5] = Min;
+}
